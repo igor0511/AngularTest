4f8aabbdd44297126f318ba3da0f4c668068bb2e
Igor Djuric <igordjuric@Igors-MacBook-Air.local>
2020-02-17 14:11:45 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import {Store} from '@ngrx/store';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'testProject';
+
+  items = [1,2,3,4];
+
+  ngAfterViewInit() {
+    console.log("View init")
+  }
+
 }
